0947f8844a86bbc8879ff6996b40ca14e5449328
Ana Carolina <102334478+AnaCarolinaMello@users.noreply.github.com>
2022-09-17 16:59:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,24 +29,30 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static('/public'))
 
+//Middleware que é usado para manter o usuário logado
 app.use(session({
     secret: "Algo",
     resave: true,
     saveUninitialized: true
 }))
 
+//Inicianização do passport para autentificação
 app.use(passport.initialize())
+//Inicianização do passport para uma sessão
 app.use(passport.session())
 
-app.use(flash())
+//Inicianização do flash
+app.use(flash()) 
 
+// Criação de variáveis globais para serem usadas no flash
 app.use((req,res,next)=>{
     res.locals.success_msg = req.flash("success_mgs")
     res.locals.error_msg = req.flash("error_mgs")
     next();
-})
+}) 
 
 mongoose.Promise = global.Promise;
+// Conexão com mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/horizonEvents").then(()=>{
     console.log("Conectado ao mongo")
 }).catch((err)=>{
@@ -57,55 +63,71 @@ app.listen(port, err =>{
     console.log(`http://localhost:${port}`)
 });
 
-
+//Requisição do arquivo com modelo da collection
 require("./models/User")
+//Passando para uma variável o modelo da collection
 const User_Cliente = mongoose.model("Usuario_Cliente")
 
 app.post("/userPerfil", async (req,res)=>{
 
+    // Array para mensagens de erros
     var erros = []
+    // Variável para verificação de email
     var arroba = "@"
+    // Variável para verificação de email
     var com = ".com"
 
+    //Verificação se a variável é vazia
     if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
         erros.push({texto:"Nome inválido"})
     }
 
+    //Verificação se a variável é vazia
     if(!req.body.username || typeof req.body.username == undefined || req.body.username == null){
         erros.push({texto:"Nome de usuário inválido"})
     }
 
+    //Verificação se a variável é vazia
     if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
         erros.push({texto:"Email inválido"})
     }
 
+    //Verificação se a variável contém tais caracteres
     if (!req.body.email.toLowerCase().includes(arroba.toLowerCase()) || !req.body.email.toLowerCase().includes(com.toLowerCase())) {
         erros.push({texto:'Email inválido'})
     }
 
+    //Verificação se a variável é vazia
     if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
         erros.push({texto:"Senha inválida"})
 
+    //Verificação se a variável tem menos de 32 caracteres
     }else if(req.body.senha.length > 32){
         erros.push({texto:"Senha precisa conter menos de 32 caracteres"})
 
+    //Verificação se a variável tem mais de 8 caractere
     }else if(req.body.senha.length < 8){
         erros.push({texto:"Senha precisa conter, no mínimo, 8 caracteres"})
     }
 
+    //Verificação se a variável é vazia
     if(!req.body.confirme_senha || typeof req.body.confirme_senha == undefined || req.body.confirme_senha == null){
         erros.push({texto:"Senha de confirmação inválida"})
 
+    //Verificação se a senha e sua confirmação batem
     }else if(req.body.senha != req.body.confirme_senha){
 
         erros.push({texto:"Senhas incompatíveis"})
     }
 
+    //Verificando se existem erros
     if(erros.length == 0){
         
         let email = req.body.email
         let user_name = req.body.username
+        //Verificando se já existe o email e username passados já existem no banco de dados  
         let conferirUser = User_Cliente.findOne({ "$or": [ { email: email }, { user_name: user_name} ] });
+        //Código se existirem
         if(conferirUser == true){
             erros.push({texto:"Emai ou nome de usuário já existentes"})
             console.log("Emai ou nome de usuário já existentes")
@@ -115,6 +137,7 @@ app.post("/userPerfil", async (req,res)=>{
                 erros: erros
             })
         }else{
+            //Setando criação de hash(tipo criptografia que não pode ser descriptografada)
             const salt =await bcrypt.genSaltSync(10)
             const senha = await req.body.senha
             const novoUserCliente = {
@@ -124,6 +147,7 @@ app.post("/userPerfil", async (req,res)=>{
                 telefone: req.body.telefone,
                 senha: bcrypt.hashSync(senha, salt)
             }
+            //Criando novo usuário
             new User_Cliente(novoUserCliente).save().then(()=>{
                 console.log("Novo usuário adicionado")
                 req.flash("success_mgs","Usuário cadastrado com sucesso")
@@ -148,6 +172,7 @@ app.post("/userPerfil", async (req,res)=>{
     }
 })
 
+//Rota para se o usuário tentar entrar direto na área de usuários
 app.get("/userPerfil", (req,res)=>{
 
     var erros = []
@@ -163,6 +188,7 @@ app.get("/userPerfil", (req,res)=>{
     }
 })
 
+//Tentativa de dar um upload em imagens no banco de dados
 app.post("/upload", (req,res)=>{
 
     var form = new formidable.IncomingForm();
@@ -195,6 +221,11 @@ app.post("/upload", (req,res)=>{
 })
 
 app.post("/userLogin", (req,res,next)=>{
+    res.redirect('/userLoginPerfil')
+})
+
+//Rota de autentificação do login
+app.post("/userLoginPerfil", (req,res,next)=>{
     passport.authenticate("local",{
         successRedirect: '/userPerfil',
         failureRedirect: '/userLogin',
